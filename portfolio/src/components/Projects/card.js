f60303c8f30a09805ebb3d756d29910225ecf7e9
pmac1687 <pmac1687@outlook.com>
2021-04-18 10:11:32 -0700

    add flip animate

--- portfolio/src/components/Projects/card.js
+++ portfolio/src/components/Projects/card.js
@@ -1,24 +1,56 @@
 import React from 'react';
+//import './AppProject.css';
 import {
   Card, CardText, CardBody, CardLink,
   CardTitle, CardSubtitle
 } from 'reactstrap';
 
 const EXPCard = (props) => {
+  function flip(e){
+    console.log(e);
+    const el =e.target;
+    if(e.target.id === 'content'){
+      console.log('content');
+    }
+    if(e.target.id === 'front'){
+      console.log('front');
+      const next = e.target.offsetParent;
+      next.style.transform = 'rotateY(180deg)';
+      next.style.transition = 'transform 0.5s';
+    }
+  }
+  function flipBack(e){
+    const el =e.target;
+    console.log(e)
+    if(el.offsetParent.id === 'back'){
+      console.log('back');
+      console.log(el.offsetParent.offsetParent.id)
+      const parent = el.offsetParent.offsetParent;
+      console.log('par', parent)
+      parent.style.transform = 'rotateY(0deg)';
+      parent.style.transition = 'transform 0.5s';
+      
+    }
+  }
   return (
-    <div style={{ width:'20vw'}}>
-      <Card style={{ backgroundColor:'#231b1b'}}>
-        <CardBody>
-          <CardTitle style={{ color:'#ddfff7'}} tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-        </CardBody>
-        <img width="100%" src={props.img} alt="Card image cap" />
-        <CardBody>
-          <CardText style={{ color: '#fff9fb'}}>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <CardLink href="#">Card Link</CardLink>
-          <CardLink href="#">Another Link</CardLink>
-        </CardBody>
-      </Card>
+    <div id='card' onMouseEnter={(e) => flip(e)} onMouseLeave={(e) => flipBack(e)} style={{ width:'20vw', paddingTop:'60vh'}}>
+      <div id='content' style={{ transition:'transform 1s', transformStyle: 'preserve-3d'}}>
+        <div id='front' style={{  backfaceVisibility: 'hidden',position:'absolute', width:'20vw', height:'50vh',backgroundColor:'black'}}>
+            hello
+        </div>
+        <Card id='back' style={{transform:'rotateY(180deg)',position:'absolute', backfaceVisibility:'hidden', backgroundColor:'#231b1b' }}>
+          <CardBody>
+            <CardTitle style={{ color:'#ddfff7'}} tag="h5">Card title</CardTitle>
+            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+          </CardBody>
+          <img width="100%" src={props.img} alt="Card cap" />
+          <CardBody>
+            <CardText style={{ color: '#fff9fb'}}>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <CardLink href="#">Card Link</CardLink>
+            <CardLink href="#">Another Link</CardLink>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
